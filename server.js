0a10fe09def364853eb1f87b117be635ea5aed69
infinitybrad <seoih415@gmail.com>
2019-06-28 22:02:17 +0900

    node express route

--- server.js
+++ server.js
@@ -1,8 +1,26 @@
 const express = require("express");
 const app = express();
 
-const port = 3000;
+const usersRoutes = require('./route/api/users');
+const profileRoutes = require('./route/api/profile');
+const postsRoutes = require('./route/api/posts');
 
+
+app.use('/api/users', usersRoutes);
+app.use('/api/profile',profileRoutes);
+app.use('/api/posts',postsRoutes);
+
+
+
+
+
+
+
+
+
+
+
+const port = 3000;
 
 app.listen(port,() => console.log(`Server running on port ${port}`));
 
