fb9084bae1da9ac83d4ea84f0e0335011da25d13
infinitybrad <seoih415@gmail.com>
2019-07-09 20:56:30 +0900

    add user reigister

--- route/api/users.js
+++ route/api/users.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router  = express.Router();
 const gravatar =  require("gravatar");
+const bcrypt = require("bcryptjs");
 
 const userModel = require('../../models/user');
 
@@ -17,37 +18,60 @@ router.get('/test',(req,res) => res.json({
 // @access Public
 router.post('/register', (req, res) =>{
 
-    // userModel
-    //     .findOne({ email: req.body.email })
-    //     .then(user => {
-    //         if(user){
-    //             return res.status(400).json({
-    //                 msg: "Email Already exists"
-    //             });
-    //         } else{
-
-                const avatar = gravatar.url(req.body.email, {
+    // 기존 모델내의 이메일 유무 체크 
+    userModel
+        .findOne({email:req.body.email})
+        .then(user => {
+            if(user)// 이메일이 있을 경우
+            {
+                return res.status(400).json({
+                    email: "email already exists"
+                });
+            }
+            else // 이메일이 없을 경우 
+            {
+                // 아바타 생성
+                 const avatar = gravatar.url(req.body.email, {
                     s:'200' ,//size
                     r:'pg' ,//Rating
                     d:'mm' //Default
                 });
+                //유저 모델 내용 사용자 입력
                 const newUser = new userModel({
                     name : req.body.name,
                     email:req.body.email,
                     avatar,
                     password:req.body.password
                 });
+                // 패스워드 암호화
+                bcrypt.genSalt(10, (err,salt) => {
+                    bcrypt.hash(newUser.password, salt, (err, hash) => {
+                        if(err) throw err;
+                        newUser.password = hash;
+                        //데이터 베이스 저장
+                        newUser
+                            .save()
+                            .then(user =>{
+                                res.status(200).json(user);
+                            })
+                            .catch(err => {
+                                res.status(400).json({
+                                    error:err
+                                });
+                            });
+
+                    })
+                })
+            }
+        })
+        .catch(err => {
+            res.status(401).json({
+                error:err
+            });
+        });
+
+               
                 
-
-                res.status(200).json(newUser);
-
-
-
-        //     }
-
-        // })
-        // .catch(err => res.json(err));
-
 
 });
 
